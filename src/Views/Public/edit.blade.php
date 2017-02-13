ced476d5b731b27b9d5643ab44ee7abde0d54721
Erik Campobadal <soc@erik.cat>
2017-02-13 16:46:49 +0100

    Tweaks

--- src/Views/Public/edit.blade.php
+++ src/Views/Public/edit.blade.php
@@ -4,6 +4,9 @@
     <div class="row">
         <div class="col-md-12 col-lg-6 offset-lg-3">
             <div class="card shadow">
+                <div class="card-header">
+                    Edit Profile ({{ $user->email }})
+                </div>
                 <div class="card-block">
                     <form method="POST" enctype="multipart/form-data">
                         {!! csrf_field() !!}
