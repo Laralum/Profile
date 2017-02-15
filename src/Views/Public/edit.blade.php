3fbdea673f21caacacb55be554a05937c43e69cd
24aitor <1aitorribaarchilla@gmail.com>
2017-02-16 00:32:38 +0100

    Fixed public view to new public layout

--- src/Views/Public/edit.blade.php
+++ src/Views/Public/edit.blade.php
@@ -1,57 +1,32 @@
 @extends('laralum::layouts.public')
 @section('title', trans('laralum_profile::profile.account_settings'))
 @section('content')
-    <div class="row">
-        <div class="col-md-12 col-lg-6 offset-lg-3">
-            <div class="card shadow">
-                <div class="card-header">
                     @lang('laralum_profile::profile.edit_profile') ({{ $user->email }})
-                </div>
-                <div class="card-block">
+                    <br><br>
                     <form method="POST" enctype="multipart/form-data">
                         {!! csrf_field() !!}
-                        <label for="profile_picture">@lang('laralum_profile::profile.profile_picture')</label>
-                        <div class="row" id="profile_picture">
-                            <div class="col-8">
-                                <label class="custom-file">
-                                    <input type="file" id="file" name="picture" value="{{old('picture')}}" class="custom-file-input">
-                                    <span class="custom-file-control"></span>
-                                </label>
-                            </div>
-                            <div class="col-4">
-                                <label class="custom-control custom-checkbox">
-                                    <input type="checkbox" name="save_picture" id="save_picture" class="custom-control-input" @if(old('save_picture',$user->hasAvatar())) checked @endif>
-                                        <span class="custom-control-indicator"></span>
-                                        <span class="custom-control-description">@lang('laralum_profile::profile.save_picture')</span>
-                                    </label>
-                                </div>
-                        </div>
+                        <label for="picture">@lang('laralum_profile::profile.profile_picture')</label>
+                        <input type="file" id="file" name="picture" value="{{old('picture')}}" class="custom-file-input">
                         <br>
-                        <div class="form-group">
-                            <label for="name">@lang('laralum::general.name')</label>
-                            <input id="name" type="text" name="name" value="{{ old('name', isset($user->name) ? $user->name : '' ) }}" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">@lang('laralum::general.password')</label>
-                            <input id="password" type="password" name="password" value="" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="password_confirmation">@lang('laralum::general.password_confirmation')</label>
-                            <input id="password_confirmation" type="password" name="password_confirmation" value="" class="form-control">
-                        </div>
-                        <br />
-                        <div class="form-group">
-                            <label for="current_password">@lang('laralum::general.current_password')</label>
-                            <input id="current_password" type="password" name="current_password" value="" class="form-control">
-                        </div>
-                        <br />
-                        <a href="#" class="btn btn-warning float-left">@lang('laralum::general.cancel')</a>
+                        <input type="checkbox" name="save_picture" id="save_picture" class="custom-control-input" @if(old('save_picture',$user->hasAvatar())) checked @endif>
+                        <span>@lang('laralum_profile::profile.save_picture')</span>
+                        <br>
+                        <label for="name">@lang('laralum::general.name')</label>
+                        <input id="name" type="text" name="name" value="{{ old('name', isset($user->name) ? $user->name : '' ) }}" class="form-control">
+                        <br>
+                        <label for="password">@lang('laralum::general.password')</label>
+                        <input id="password" type="password" name="password" value="">
+                        <br>
+                        <label for="password_confirmation">@lang('laralum::general.password_confirmation')</label>
+                        <input id="password_confirmation" type="password" name="password_confirmation" value="">
+                        <br>
+                        <label for="current_password">@lang('laralum::general.current_password')</label>
+                        <input id="current_password" type="password" name="current_password" value="">
+                        <br>
+
+                        <a href="#">@lang('laralum::general.cancel')</a>
                         <button type="submit" class="btn btn-success float-right clickable">@lang('laralum::general.update')</button>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
 @section('js')
     <script>
--- src/Views/Public/profile.blade.php
+++ src/Views/Public/profile.blade.php
@@ -1,21 +1,8 @@
 @extends('laralum::layouts.public')
 @section('title', trans('laralum_profile::profile.profile'))
 @section('content')
-    <div class="row">
-        <div class="col-md-12 col-lg-6 offset-lg-3">
-            <div class="card shadow">
-                <div class="card-block">
-                        {!! csrf_field() !!}
-                        <center>
-                            <img src="{{ $user->avatar() }}" class="rounded-circle" alt="@lang('laralum_profile::profile.profile_picture')" style="max-width=100px;max-height:100px">
-                            <br><br>
-                            <p style="font-size:20px;">{{ $user->name }}</p>
-                            <p class="text-muted">{{ $user->email }}</p>
-                            <br><br>
-                            <a href="{{ route('laralum_public::profile.edit') }}" class="btn btn-info">@lang('laralum::general.edit')</a>
-                        </center>
-                </div>
-            </div>
-        </div>
-    </div>
+    <img src="{{ $user->avatar() }}" alt="@lang('laralum_profile::profile.profile_picture')" style="max-width=100px;max-height:100px">
+    <p style="font-size:20px;">{{ $user->name }}</p>
+    <p>{{ $user->email }}</p>
+    <a href="{{ route('laralum_public::profile.edit') }}">@lang('laralum::general.edit')</a>
 @endsection
