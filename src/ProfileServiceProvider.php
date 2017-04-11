e5db9cf34c4eded7ba6979eec6443bb669628bd0
24aitor <contact@aitorriba.com>
2017-04-11 15:35:11 +0200

    Typo

--- src/ProfileServiceProvider.php
+++ src/ProfileServiceProvider.php
@@ -21,7 +21,7 @@ class ProfileServiceProvider extends ServiceProvider
         }
 
         $this->publishes([
-            __DIR__.'/Views/public' => resource_path('views/vendor/Laralum/Profile'),
+            __DIR__.'/Views/public' => resource_path('views/vendor/laralum/profile'),
         ], 'laralum_profile');
 
         $this->loadViewsFrom(__DIR__.'/Views', 'laralum_profile');
